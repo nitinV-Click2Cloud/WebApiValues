6320df3ea3ab101a3ada144365954a4be14a8808
nitinv-click2cloud <nitin.vaswani@click2cloud.net>
2016-02-01 11:34:04 +0530

    First commit

---
+++ Controllers/ProjectMasterController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using WebApplication4.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication4.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectMasterController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<WebApplication4.Models.tblProject_Master> Get()
+        {
+            return new tblProject_Master[] { };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public tblProject_Master Get(int id)
+        {
+            //return new tblProject_Master();
+            SqlDataReader reader = null;
+            SqlConnection myConnection = new SqlConnection();
+            myConnection.ConnectionString = @"Data Source=192.168.0.110;Initial Catalog=TTSHTemp;User ID=sa;Password=ROOT#123";
+            SqlCommand sqlCmd = new SqlCommand();
+            //sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.CommandText = "Select Top 1 * from tblProject_Master where i_Id=" + id + "";
+            sqlCmd.Connection = myConnection;
+            myConnection.Open();
+            reader = sqlCmd.ExecuteReader();
+            tblProject_Master project_Master = null;
+            while (reader.Read())
+            {
+                project_Master = new tblProject_Master();
+                project_Master.i_ID= Int32.Parse(reader.GetValue(0).ToString());
+                project_Master.s_Display_Project_ID = reader.GetValue(1).ToString();
+                project_Master.s_Project_Title = reader.GetValue(2).ToString(); ;
+                project_Master.s_Short_Title = reader.GetValue(3).ToString();
+            }
+            myConnection.Close();
+            return project_Master;
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/tblProject_Master.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class tblProject_Master
+    {
+        public int i_ID
+        {
+            get;
+            set;
+        }
+     
+        public string s_Display_Project_ID
+        {
+            get;
+            set;
+        }
+   
+        public string s_Project_Title
+        {
+            get;
+            set;
+        }
+     
+        public string s_Short_Title
+        {
+            get;
+            set;
+        }
+       
+        public int i_Project_Category_ID
+        {
+            get;
+            set;
+        }
+     
+        public int i_Project_Type_ID
+        {
+            get;
+            set;
+        }
+       
+        public int i_Project_Subtype_ID
+        {
+            get;
+            set;
+        }
+    
+        public bool b_Collaboration_Involved
+        {
+            get;
+            set;
+        }
+      
+        public bool b_StartBy_TTSH
+        {
+            get;
+            set;
+        }
+     
+        public bool b_Funding_req
+        {
+            get;
+            set;
+        }
+     
+        public bool b_Ischild
+        {
+            get;
+            set;
+        }
+    
+        public int i_Parent_ProjectID
+        {
+            get;
+            set;
+        }
+       
+        public string s_Project_Alias1
+        {
+            get;
+            set;
+        }
+     
+        public string s_Project_Alias2
+        {
+            get;
+            set;
+        }
+    
+        public string s_Project_Desc
+        {
+            get;
+            set;
+        }
+     
+        public int b_IsFeasible
+        {
+            get;
+            set;
+        }
+    
+        public bool b_Isselected_project
+        {
+            get;
+            set;
+        }
+      
+        public string s_IRB_No
+        {
+            get;
+            set;
+        }
+      
+        public string s_Research_IO
+        {
+            get;
+            set;
+        }
+        public string s_Research_IP
+        {
+            get;
+            set;
+        }
+        public string s_CreatedBy_ID
+        {
+            get;
+            set;
+        }
+        public string s_ModifyBy_ID
+        {
+            get;
+            set;
+        }
+        public DateTime dt_Created_Date
+        {
+            get;
+            set;
+        }
+        public DateTime dt_Modify_Date
+        {
+            get;
+            set;
+        }
+
+    }
+}
